022d41d5202b3abf62435588ad227566f9cbb80e
Ali Haider <64097260+Haider-cloud-creater@users.noreply.github.com>
2021-05-29 21:07:02 +0500

    Print Statement

--- AddFunc.java
+++ AddFunc.java
@@ -2,7 +2,7 @@ package Assignment2;
 
 public class AddFunc {
 	//System.out.Println("Assgnment 2");
-	
+	//System.out.Println("Assgnment 2");
 	public static long Factorial(int n) {
 	if(n==0) {
 		return 1;    
